6fd2401d90256e5451e4e554cc0ab5ad4d4276a6
beanstown106 <nbenis106@gmail.com>
2016-01-14 05:56:50 -0500

    Fingerprint authentication vibration [1/2]
    @xyyx: adapted to 8.1
    
    Change-Id: Ieed52af28462da7507bcae01e13744f7dc5ed32c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4258,6 +4258,13 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /**
+         * whether to enable or disable vibration on succesful fingerprint auth
+         *
+         * @hide
+         */
+        public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
