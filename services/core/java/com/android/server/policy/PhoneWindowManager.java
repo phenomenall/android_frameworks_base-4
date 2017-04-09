e4427b027906fad3d2b45972dbf4975919d393f6
maxwen <max.weninger@gmail.com>
2017-04-09 16:40:52 +0200

    [1/2] base: featureize ro.recents.grid and Go Layout
    thanks google for coding like a 1st grader by adding public
    member variables and making this more work then needed
    
    Change-Id: Idf9c9fcc453296244ce7b5b84f412245d55ffc66
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -987,6 +987,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.Global.getUriFor(
                     Settings.Global.POLICY_CONTROL), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.RECENTS_LAYOUT_STYLE), false, this,
+                    UserHandle.USER_ALL);
             updateSettings();
         }
 
