de2400e9d50f32444450bf28dcb71d8dec367d8b
Alex <alex@dirtyunicorns.com>
2018-05-23 23:14:17 -0500

    Allow to disable the Wallpaper tint [1/2]
    In Oreo, Google introduced an API to easily extract the color of wallpaper.
    This paved the way for a lot of neat things, some of which we took advantage
    of with Pulse. Unfortunately this also meant that Gogole went ahead and applied
    a tint based on the user's wallpaper in a few areas that really should of never
    got a tint. The power menu, recents, keyguard and notification shade all got
    tagged for this.
    
    This commit will allow the user to toggle the wallpaper tint.
    
    Default behavior is still in play so we're not shifting away from the AOSP look
    and feel.
    
    Change-Id: Iae5f0223fe5fb0e8c78e4fc81c907a73916b6a8d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3941,6 +3941,30 @@ public final class Settings {
         public static final String QS_SHOW_BRIGHTNESS_ICON = "qs_show_brightness_icon";
 
         /**
+         * Whether to display the wallpaper tint on the power menu
+         * @hide
+         */
+        public static final String WALLPAPER_POWER_MENU_TINT = "wallpaper_power_menu_tint";
+
+        /**
+         * Whether to display the wallpaper tint in recents
+         * @hide
+         */
+        public static final String WALLPAPER_RECENTS_TINT = "wallpaper_recents_tint";
+
+        /**
+         * Whether to display the wallpaper tint in the notification shade
+         * @hide
+         */
+        public static final String WALLPAPER_NOTIFICATION_SHADE_WALLPAPER_TINT = "wallpaper_notification_shade_wallpaper_tint";
+
+        /**
+         * Whether to display the wallpaper tint in the keyguard
+         * @hide
+         */
+        public static final String WALLPAPER_KEYGUARD_WALLPAPER_TINT = "wallpaper_keyguard_wallpaper_tint";
+
+        /**
          * Select which lockscreen date style to display
          * @hide
          */
