DarkKat-Rom/android_frameworks_base

Status bar: Clock styles, (1/2):
Status bar: Clock styles, (1/2):

- Clock position:
  - Right
  - Centered
  - Hidden
- Show/hide Clock seconds
- Show/hide custom date left to the clock:
  - Format
  - Style:
    - Normal
    - Lowercase
    - Uppercase
  - Size , (small or big)

Also remove the clock preference from SystemUI Tuner

This also includes AOSPA commit:
SystemUI: Fix No AM/PM showing in 12-hour-format:
https://github.com/AOSPA/android_frameworks_base/commit/018810c8541b0be0b655546d01966261171f8d79#diff-1fa71c270d063a48f921f0760d10c7f3

core/java/android/provider/Settings.java
@@ -3939,6 +3939,79 @@ public final class Settings {
         public static final String DETAILED_WEATHER_CONDITION_ICON = "detailed_weather_condition_icon";
 
         /**
+         * Clock (and date) position on the status bar
+         * 0: Right clock
+         * 1: Center clock
+         * 2: Hidden clock
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_DATE_POSITION = "status_bar_clock_date_position";
+
+        /**
+         * Wether to show clock seconds on the status bar
+         * 0: Hide
+         * 1: Show
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_SHOW_SECONDS = "status_bar_clock_show_seconds";
+
+        /**
+         * Wether to show the date left to the clock on the status bar
+         * 0: Hide
+         * 1: Show
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_SHOW_DATE = "status_bar_clock_show_date";
+
+        /**
+         * The DateFormat string for the date on the status bar
+         * - "dd/MM/yy"
+         * - "MM/dd/yy"
+         * - "yyyy-MM-dd"
+         * - "yyyy-dd-MM"
+         * - "dd-MM-yyyy"
+         * - "MM-dd-yyyy"
+         * - "MMM dd"
+         * - "MMM dd, yyyy"
+         * - "MMMM dd, yyyy"
+         * - "EEE"
+         * - "EEE dd"
+         * - "EEE dd/MM"
+         * - "EEE MM/dd"
+         * - "EEE dd MMM"
+         * - "EEE MMM dd"
+         * - "EEE MMMM dd"
+         * - "EEEE dd/MM"
+         * - "EEEE MM/dd"
+         * - "Custom date format"
+         * default: "EEE", (day name in week (short))
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_DATE_FORMAT = "status_bar_clock_date_format";
+
+        /**
+         * The date style on the status bar
+         * 0 - Regular (case insensitive)
+         * 1 - Lowercase
+         * 2 - Uppercase
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_DATE_STYLE = "status_bar_clock_date_style";
+
+        /**
+         * Whether to show the date small in status bar
+         * 0: Normal
+         * 1: Small
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_DATE_SIZE_SMALL = "status_bar_clock_date_sizes_mall";
+
+        /**
          * Turn on ambient display on notification
          * (Used for devices without a proximity sensor)
          * 0 - Always

phenomenall/android_frameworks_base-4

core/java/android/provider/Settings.java
@@ -3941,6 +3941,79 @@ public final class Settings {
         public static final String QS_SHOW_BRIGHTNESS_ICON = "qs_show_brightness_icon";
 
         /**
+         * Clock (and date) position on the status bar
+         * 0: Right clock
+         * 1: Center clock
+         * 2: Hidden clock
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_DATE_POSITION = "status_bar_clock_date_position";
+
+        /**
+         * Wether to show clock seconds on the status bar
+         * 0: Hide
+         * 1: Show
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_SHOW_SECONDS = "status_bar_clock_show_seconds";
+
+        /**
+         * Wether to show the date left to the clock on the status bar
+         * 0: Hide
+         * 1: Show
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_SHOW_DATE = "status_bar_clock_show_date";
+
+        /**
+         * The DateFormat string for the date on the status bar
+         * - "dd/MM/yy"
+         * - "MM/dd/yy"
+         * - "yyyy-MM-dd"
+         * - "yyyy-dd-MM"
+         * - "dd-MM-yyyy"
+         * - "MM-dd-yyyy"
+         * - "MMM dd"
+         * - "MMM dd, yyyy"
+         * - "MMMM dd, yyyy"
+         * - "EEE"
+         * - "EEE dd"
+         * - "EEE dd/MM"
+         * - "EEE MM/dd"
+         * - "EEE dd MMM"
+         * - "EEE MMM dd"
+         * - "EEE MMMM dd"
+         * - "EEEE dd/MM"
+         * - "EEEE MM/dd"
+         * - "Custom date format"
+         * default: "EEE", (day name in week (short))
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_DATE_FORMAT = "status_bar_clock_date_format";
+
+        /**
+         * The date style on the status bar
+         * 0 - Regular (case insensitive)
+         * 1 - Lowercase
+         * 2 - Uppercase
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_DATE_STYLE = "status_bar_clock_date_style";
+
+        /**
+         * Whether to show the date small in status bar
+         * 0: Normal
+         * 1: Small
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_DATE_SIZE_SMALL = "status_bar_clock_date_size_small";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate