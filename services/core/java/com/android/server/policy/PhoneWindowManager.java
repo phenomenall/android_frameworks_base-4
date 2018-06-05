abb0128cac7452968d4e0d29c68733af14156078
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-06-05 13:47:23 -0300

    PhoneWindowManager: Fix haptic on navbar when hwkeys is disabled
    Change-Id: I01fcd6efe5b70358d8b13090cf1b09935da0c7cf

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6126,6 +6126,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         final boolean down = event.getAction() == KeyEvent.ACTION_DOWN;
         final boolean canceled = event.isCanceled();
         final int keyCode = event.getKeyCode();
+        final boolean virtualKey = event.getDeviceId() == KeyCharacterMap.VIRTUAL_KEYBOARD;
 
         final boolean isInjected = (policyFlags & WindowManagerPolicy.FLAG_INJECTED) != 0;
 
@@ -6203,9 +6204,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         boolean useHapticFeedback = down
                 && (policyFlags & WindowManagerPolicy.FLAG_VIRTUAL) != 0
-                && event.getRepeatCount() == 0
-                && !isHwKeysDisabled()
-                && !keyguardOn();
+                && event.getRepeatCount() == 0;
+
+        if (!virtualKey){
+            if (isHwKeysDisabled()){
+                useHapticFeedback = false;
+            }else if (keyCode != KeyEvent.KEYCODE_BACK && keyguardOn()){
+                useHapticFeedback = false;
+            }
+        }
 
         // Specific device key handling
         if (mDeviceKeyHandler != null) {
