ab2459d30120352da0b8a8fbc99a9133aefb673a
ezio84 <brabus84@gmail.com>
2018-06-08 21:48:17 -0300

    Unlock per-device lockscreen clock padding
    Set dozing_big_clock_padding to a bigger value than 0.33 (e.g. 0.38)
    to move the clock down if it gets cut on your device
    
    Change-Id: I413924dfa8d349e24f4ab7dc8ad32b1c58f64236

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardClockPositionAlgorithm.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardClockPositionAlgorithm.java
@@ -184,7 +184,7 @@ public class KeyguardClockPositionAlgorithm {
     }
 
     public void setClockSelection(int style) {
-        mIsBigClock = style == 3;
+        mIsBigClock = style == 2 || style == 3 || style == 4;
     }
 
     private float burnInPreventionOffsetY() {
